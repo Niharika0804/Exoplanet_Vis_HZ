ec2d9b2047b3b271425882734c0b6108d307cfc6
Niharika0804 <123380924+Niharika0804@users.noreply.github.com>
2024-09-21 11:56:59 +0530

    Update vis.py

--- vis.py
+++ vis.py
@@ -11,5 +11,17 @@ def index():
     plots_html = generate_plots(df)
     return render_template('index.html', plots=plots_html)
 
+
+@app.route('/<star_system>')
+def show_star_system(star_system):
+    star_data = df[df['S_NAME'] == star_system]
+
+    if star_data.empty:
+        return f"No data available for {star_system}", 404
+
+    plots_html = generate_plots(star_data)  #function call from test.py for star system
+    return render_template('star_system.html', plots=plots_html, star_system=star_system)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
