be54d0beba3f171bb68d68c8e170c228a63d7d9c
Niharika0804 <123380924+Niharika0804@users.noreply.github.com>
2024-11-07 05:13:14 +0530

    Update vis.py

--- vis.py
+++ vis.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 import pandas as pd
-from test import generate_plots  # Importing all the plotting functions
+from test8 import generate_plots  # Importing all the plotting functions
+from dash_app import create_dash_app #importing the dash_app.py file
 
 app = Flask(__name__)
+
+dash_app = create_dash_app(app)
 
 df = pd.read_csv("hwc_3d_data.csv")
 
@@ -18,10 +21,19 @@ def show_star_system(star_system):
 
     if star_data.empty:
         return f"No data available for {star_system}", 404
+    star_info = star_data.iloc[0]
 
-    plots_html = generate_plots(star_data)  #function call from test.py for star system
-    return render_template('star_system.html', plots=plots_html, star_system=star_system)
+    return render_template('star_systems.html', star_system=star_system, star_info=star_info)
+
+@app.route('/<star_system>_vis')
+def show_star_system_vis(star_system):
+    star_data = df[df['S_NAME'] == star_system]
+
+    if star_data.empty:
+        return f"No data available for {star_system}", 404
+
+    plots_html = generate_plots(star_data)  # Call the function from plotting.py for this star system
+    return render_template('star_system_vis.html', plots=plots_html, star_system=star_system)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
